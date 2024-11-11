7e6115a0cb78cdbe54cfc7d74f494389b7cea73b
jheremy02 <jheremy26@gmail.com>
2024-11-10 23:36:25 -0500

    correcion de estilos de notificaciones

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         pauseOnFocusLoss
         draggable
         pauseOnHover
-        theme='dark'
+        theme={themeUi}
         transition:Bounce
       />
       <Routes>
